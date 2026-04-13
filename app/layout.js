019dee41b222c015cd88360d2fe89914c5fa0ae9
vineet-proptiger <vineet.mishra@proptiger.com>
2026-04-13 16:28:14 +0530

    GTAG fixes

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import './globals.css'
 import { Open_Sans, Montserrat, Cormorant_Garamond } from 'next/font/google'
 import localFont from 'next/font/local'
 import { GoogleTagManager } from '@next/third-parties/google'
-import Script from 'next/script'
 
 const openSans = Open_Sans({
   subsets: ['latin'],
@@ -39,14 +38,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <GoogleTagManager gtmId="GTM-NWR7SPL4" />
+      <GoogleTagManager gtmId="GTM-575H8R87" />
       <body className={`${openSans.variable} ${montserrat.variable} ${cormorant.variable} ${nephilm.variable} font-sans text-dark antialiased`}>
-        <Script id="gtag-init" strategy="beforeInteractive">
-          {`window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());`}
-        </Script>
-        {children}
+{children}
       </body>
     </html>
   )
